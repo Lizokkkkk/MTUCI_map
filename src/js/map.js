4b2e80679244bf59ebdb0f1f9e97e0a488f670da
Liza <yemelyanova@list.ru>
2023-05-03 18:08:39 +0300

    add event listeners for map elements

--- src/js/map.js
+++ src/js/map.js
@@ -38,3 +38,41 @@ links.forEach((item) => {
 
   });
 });
+
+mapLinks.forEach((item) => {
+    item.addEventListener("mouseenter", (e) => {
+      let self = e.currentTarget;
+      let selfClass = self.getAttribute("id");
+  
+      links.forEach((el) => {
+        if (el.getAttribute("class") != selfClass) {
+          el.style.background = "transparent";
+          el.querySelector("a").style.color = "#EDEDED";
+        }
+      });
+  
+      mapLinks.forEach((el) => {
+          if (el.getAttribute("id") != selfClass) {
+              el.style.display = "none";
+          }
+      });
+    });
+  
+    item.addEventListener("mouseleave", (e) => {
+      let self = e.currentTarget;
+      let selfClass = self.getAttribute("id");
+  
+      links.forEach((el) => {
+        el.style.background = "";
+        el.style.tr
+        el.querySelector("a").style.color = "";
+      });
+  
+      mapLinks.forEach((el) => {
+          if (el.getAttribute("id") != selfClass) {
+              el.style.display = "";
+          }
+      });
+  
+    });
+  });
